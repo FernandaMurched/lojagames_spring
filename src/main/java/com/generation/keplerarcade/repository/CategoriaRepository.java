a219e812cfff48152a61daa207ba8cbffa965eb0
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-05-21 17:21:14 -0300

    :wrench: fix: Ajuste da CategoriaRepository

--- src/main/java/com/generation/keplerarcade/repository/CategoriaRepository.java
+++ src/main/java/com/generation/keplerarcade/repository/CategoriaRepository.java
@@ -3,12 +3,12 @@ package com.generation.keplerarcade.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.generation.keplerarcade.model.Categoria;
 
-@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	
-	public List<Categoria> findAllByNomeContainingIgnoreCase(String descricao);
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+	public List<Categoria> findAllByClassificacaoContainingIgnoreCase(String classificacao);
+
 }
