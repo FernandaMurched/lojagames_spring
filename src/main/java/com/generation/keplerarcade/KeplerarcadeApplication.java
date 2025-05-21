731b06e713be65702530476bb4a875514d8dd65d
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-05-21 10:28:40 -0300

    :tada: Commit Inicial - Projeto Loja de Games

---
+++ src/main/java/com/generation/keplerarcade/KeplerarcadeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.keplerarcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeplerarcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeplerarcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/keplerarcade/KeplerarcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.keplerarcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeplerarcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
