bb80cd631ae02d3d90a10628e968dd866f760541
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-05-21 12:07:04 -0300

    :sparkles: feat: Construção das Classes Produto e Categoria

---
+++ src/main/java/com/generation/keplerarcade/model/Produto.java
@@ -0,0 +1,93 @@
+package com.generation.keplerarcade.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "É obrigatório preencher o atributo nome!")
+	@Size(min = 5, max = 100, message = "O atributo título deve ter no mínimo 5 e no máximo 100 caracteres.")
+	private String nome;
+	
+	@Column(length = 50)
+	@NotBlank(message = "É obrigatório preencher o atributo tipo!")
+	@Size(min = 5, max = 50, message = "O atributo tipo deve ter no mínimo 5 e no máximo 50 caracteres.")
+	private String tipo; 
+	
+	private Double preco;
+	
+	@Column(length = 5000)
+	private String imgUrl;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	
+	
+
+}
